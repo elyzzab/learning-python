9b68de16cbb88bb88a720794dba66dc3b6ebc66c
Elyzza <45059345+elyzzab@users.noreply.github.com>
2022-08-09 20:03:56 -0500

    Create block-letters.py

---
+++ hello-world/block-letters.py
@@ -0,0 +1,13 @@
+# Elyzza B
+# I like the color periwinkle.
+
+name = """
+ EEEEE  BBBB  
+ E      B   B 
+ E      B   B 
+ EEE    BBBB  
+ E      B   B 
+ E      B   B 
+ EEEEE  BBBB  
+"""
+print(name)
